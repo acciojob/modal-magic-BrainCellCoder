e79f6945b5200c8c95fb24f95bfb51dcb034391a
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 10:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const modal = document.getElementById('modal');
+const openModal = document.getElementById('openModal');
+const closeModal = document.getElementById('closeModal');
+openModal.onclick = function() {
+    modal.style.display = 'block';
+}
+closeModal.onclick = function() {
+    modal.style.display = 'none';
+}
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
